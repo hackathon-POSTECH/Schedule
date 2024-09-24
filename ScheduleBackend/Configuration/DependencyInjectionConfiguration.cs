f243b3767bad9f0814782cb66e2946e4db252e59
Marlon <102394517+MarlonMRibeiro@users.noreply.github.com>
2024-09-23 22:14:53 -0300

    First Commit - Create Project

---
+++ ScheduleBackend/Configuration/DependencyInjectionConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using ScheduleDomain.Interface;
+using ScheduleInfra.Repository;
+
+namespace ScheduleApi.Configuration;
+
+public static class DependencyInjectionConfiguration
+{
+    public static void AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IConsultRepository, ConsultRepository>();
+    }
+}
+
---
+++ ScheduleInfra/ScheduleContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScheduleDomain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleInfra;
+
+public class ScheduleContext : DbContext
+{
+    public ScheduleContext(DbContextOptions<ScheduleContext> options) : base(options) { }
+
+    public DbSet<Consult> Consults { get; set; }
+    public DbSet<DoctorSchedule> DoctorSchedules { get; set; }
+    public DbSet<Schedule> Schedules { get; set; }
+}
+
